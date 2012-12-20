cff982ebb811bc492d5e2700d9daee65ca91b665
Shao-Chung Chen <dannvix@gmail.com>
2012-12-20 23:28:14 +0800

    refine test scripts

--- test/test.rb
+++ test/test.rb
@@ -1,41 +1,27 @@
 require "../Keccak"
 
-VECTORS = [[  "0", 1024,  576,   0],
-           ["224", 1152,  448, 224],
-           ["256", 1088,  512, 256],
-           ["384",  832,  768, 384],
-           ["512",  576, 1024, 512]]
-FILE_TYPES = ["Short", "Long"]
+# hash types: [r, c, n]
+TYPES = [[1024, 576, 0], [1152, 448, 224], [1088, 512, 256], [832, 768, 384], [576, 1024, 512]]
 
 keccak = Keccak.new
-VECTORS.each do |vector|
-  suffix, r, c, n = vector
+TYPES.each do |r, c, n|
+  ["short", "long"].each do |case_t|
+    pathname = "vectors/#{case_t}_#{n}.csv"
 
-  FILE_TYPES.each do |file_type|
-    pathname = "vectors/#{file_type}MsgKAT_#{suffix}.txt"
-    test_cases = File.read pathname
-    
-    len, msg, md = nil, nil, nil
     counter = 0
-    test_cases.split("\n").each do |line|
-      len = line.split(" = ").last.chomp.to_i if line.start_with? "Len"
-      msg = line.split(" = ").last.chomp if line.start_with? "Msg"
-
-      if line.start_with? "MD" or line.start_with? "Squeezed"
-        md = line.split(" = ").last.chomp.downcase
-        n = (md.length / 2) * 8 if n == 0
-
-        bytes = msg.chars.each_slice(2).to_a.map{|b| b.join.to_i(16).chr }.join
-        my_md = keccak.hexdigest(bytes, r, c, n, len)
-
-        counter += 1
-        if not md == my_md
-          puts "#{pathname.ljust(32)}: Failed :("
-          exit(1)
-        end
+    test_cases = File.open(pathname).each do |line|
+      len, msg, md = line.split(",")
+      bytes = msg.chars.each_slice(2).to_a.map{|b| b.join.to_i(16).chr }.join
+      _n = (n == 0) ? (md.length / 2 * 8) :  n
+      my_md = keccak.hexdigest(bytes, r, c, _n, len.to_i)
+   
+      counter += 1
+      if not md.chomp == my_md
+        puts "#{pathname}: failed :("
+        exit(1)
       end
     end
 
-    puts "#{pathname.ljust(32)}: Passed (#{counter} hashes)"
+    puts "#{pathname}: passed (#{counter} hashes)"
   end
 end
