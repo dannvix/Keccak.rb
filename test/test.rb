940f4319a1b73a36a5b009ec35e698ca7d215064
Shao-Chung Chen <dannvix@gmail.com>
2012-12-20 23:53:54 +0800

    rewrite test scripts with Ruby's built-in unit test tool

--- test/test.rb
+++ test/test.rb
@@ -1,27 +1,26 @@
+require "test/unit"
 require "../Keccak"
 
-# hash types: [r, c, n]
-TYPES = [[1024, 576, 0], [1152, 448, 224], [1088, 512, 256], [832, 768, 384], [576, 1024, 512]]
+class KeccakUnitTest < Test::Unit::TestCase
+  # Keccak hash types: [r, c, n]
+  TYPES = [[1024, 576, 0], [1152, 448, 224], [1088, 512, 256], [832, 768, 384], [576, 1024, 512]]
 
-keccak = Keccak.new
-TYPES.each do |r, c, n|
-  ["short", "long"].each do |case_t|
-    pathname = "vectors/#{case_t}_#{n}.csv"
-
-    counter = 0
-    test_cases = File.open(pathname).each do |line|
-      len, msg, md = line.split(",")
-      bytes = msg.chars.each_slice(2).to_a.map{|b| b.join.to_i(16).chr }.join
-      _n = (n == 0) ? (md.length / 2 * 8) :  n
-      my_md = keccak.hexdigest(bytes, r, c, _n, len.to_i)
+  # create test cases dynamically, one csv per case
+  TYPES.each do |r, c, n|
+    ["short", "long"].each do |case_t|
+      define_method("test_#{case_t}_#{n}") do
+        keccak = Keccak.new
+        pathname = "vectors/#{case_t}_#{n}.csv"
+        test_cases = File.open(pathname).each do |line|
+          len, msg, md = line.split(",")
+          bytes = msg.chars.each_slice(2).to_a.map{|b| b.join.to_i(16).chr }.join
+          _n = (n == 0) ? (md.length / 2 * 8) :  n
+          my_md = keccak.hexdigest(bytes, r, c, _n, len.to_i)
    
-      counter += 1
-      if not md.chomp == my_md
-        puts "#{pathname}: failed :("
-        exit(1)
+          assert_equal(md.chomp, my_md)
+        end
       end
     end
-
-    puts "#{pathname}: passed (#{counter} hashes)"
   end
+
 end
