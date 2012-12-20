cee5941ff22b1d9cd4034da84d6c1455f6193e90
Shao-Chung Chen <dannvix@gmail.com>
2012-12-20 15:42:17 +0800

    add official test cases

---
+++ test/test.rb
@@ -0,0 +1,38 @@
+require "../Keccak"
+
+VECTORS = [[  "0", 1024,  576,   0],
+           ["224", 1152,  448, 224],
+           ["256", 1088,  512, 256],
+           ["384",  832,  768, 384],
+           ["512",  576, 1024, 512]]
+FILE_TYPES = ["Short", "Long"]
+
+keccak = Keccak.new
+VECTORS.each do |vector|
+  suffix, r, c, n = vector
+
+  FILE_TYPES.each do |file_type|
+    pathname = "vectors/#{file_type}MsgKAT_#{suffix}.txt"
+    test_cases = File.read pathname
+    
+    len, msg, md = nil, nil, nil
+    counter = 0
+    test_cases.split("\n").each do |line|
+      len = line.split(" = ").last.chomp.to_i if line.start_with? "Len"
+      msg = line.split(" = ").last.chomp if line.start_with? "Msg"
+
+      if line.start_with? "MD" or line.start_with? "Squeezed"
+        md = line.split(" = ").last.chomp
+        my_md = keccak.keccak([len, msg], r, c, n)
+
+        counter += 1
+        if not md.slice(0, my_md.length) == my_md
+          puts "#{pathname.ljust(32)}: Failed :("
+          exit(1)
+        end
+      end
+    end
+
+    puts "#{pathname.ljust(32)}: Passed (#{counter} hashes)"
+  end
+end
